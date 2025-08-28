9583f44d6b83b6946421d082fdad0156a3d94839
Angellunaa <92132056+Angellunaa@users.noreply.github.com>
2025-08-28 08:46:25 -0600

    Add first EF migration

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,9 +1,8 @@
-using System;
+namespace DatingApp2025.API.Entities;
 
 public class AppUser
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
-    public required string DisplayName { get; set; } = string.Empty;
-    public required string Email { get; set; } = string.Empty;
-
+    public required string DisplayName { get; set; }
+    public required string Email { get; set; }
 }
