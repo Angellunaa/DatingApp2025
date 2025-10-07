4ab114456940119b1c5a7c50284775378cd86c74
Angellunaa <angelluna996@gmail.com>
2025-10-06 19:56:33 -0600

    Parcial02 - Add Exception Middleware

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ using API.Interfaces;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,6 +35,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader()
             .AllowAnyMethod()
             .WithOrigins(
