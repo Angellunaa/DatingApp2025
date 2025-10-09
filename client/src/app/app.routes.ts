281cd3f6c62e1a22b649452b2b472c78cd389e04
Angellunaa <angelluna996@gmail.com>
2025-10-09 07:48:28 -0600

    Parcial02 - Update server error page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,20 +7,22 @@ import { Messages } from '../features/messages/messages';
 import { authGuard } from '../core/guard/auth-guard';
 import { TestErrors } from '../features/test-errors/test-errors';
 import { NotFound } from '../shared/errors/not-found/not-found';
+import { ServerError } from '../shared/errors/server-error/server-error';
 
 export const routes: Routes = [
-    { path: "", component: Home},
+    { path: "", component: Home },
     {
         path: "",
-        runGuardsAndResolvers:"always",
+        runGuardsAndResolvers: "always",
         canActivate: [authGuard],
         children: [
-            { path: "members", component: MemberList},
+            { path: "members", component: MemberList },
             { path: "members/{id}", component: MemberDetail },
-            { path: "lists", component: Lists},
+            { path: "lists", component: Lists },
             { path: "messages", component: Messages },
         ]
     },
     { path: "errors", component: TestErrors },
+    { path: "server-error", component: ServerError },
     { path: "**", component: NotFound }
 ];
