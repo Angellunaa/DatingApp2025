da79f0cd50e681af24a841f229854d88783852a5
Angellunaa <angelluna996@gmail.com>
2025-10-20 07:46:45 -0600

    Parcial02 - Add Member repository

--- API/Interfaces/IMembersRepository.cs
+++ API/Interfaces/IMembersRepository.cs
@@ -8,6 +8,6 @@ public interface IMembersRepository
     void Update(Member member);
     Task<bool> SaveAllAsync();
     Task<IReadOnlyList<Member>> GetMembersAsync();
-    Task<Member> GetMemberAsync(string id);
+    Task<Member?> GetMemberAsync(string id);
     Task<IReadOnlyList<Photo>> GetPhotosAsync(string memberId);
 }
