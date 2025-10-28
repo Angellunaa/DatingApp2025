3c32e11cb03133a20012a56df7efee7928ef87e1
Angellunaa <92132056+Angellunaa@users.noreply.github.com>
2025-10-27 20:40:47 -0600

    Parcial02 (#3)
    * Parcial02 - Add password hash salt
    
    * Parcial02 - Add BaseApiController
    
    * Parcial02 - Add AccountController and DTO
    
    * Parcial02 - Add login endpoint
    
    * Parcial02 -  Add Token Service
    
    * Parcial02 - Add Token Service CreateToken
    
    * Parcial02 - Add token to login/register response
    
    * Parcial02 - Add JWT Middleware
    
    * Parcial02 - Add JWT Middleware
    
    * Parcial02 - Add AppUser extension method
    
    * Parcial02 - Folder structure and Nav component
    
    * Parcial02 - Add styling to nav bar
    
    * Parcial02 - Add Temaplate Forms in Angular
    
    * Parcial02 - Add Account Service to SPA
    
    * Parcial02 - Add Angular Conditionals to display DOM elements
    
    * Parcial02 - Add Pesistence to Login User with Signals
    
    * Parcial02 - Add Home Page
    
    * Parcial02 - Add Register form
    
    * Parcial02 - Add Parent-Child communication
    
    * Parcial02 - Add Child communication
    
    * Parcial02 - Remove Parent-Child Communication
    
    * Parcial02 - Add hook to API register
    
    * Parcial02 - Add Routing
    
    * Parcial02 - Add nav links
    
    * Parcial02 - Add nav links
    
    * Parcial02 - Add Toast Service
    
    * Parcial02 - Add Toast Service
    
    * Parcial02 - Add Route Guard
    
    * Parcial02 - Add routes to guard
    
    * Parcial02 - Add app init
    
    * Parcial02 - Add routes with view transitions
    
    * Parcial02 - Add Exception Middleware
    
    * Parcial02 - Add errors
    
    * Parcial02 - Add Http Intercpetor
    
    * Parcial02 - Add error handing, not-found and server error pages
    
    * Parcial02 - Update server error page
    
    * Parcial02 - Add Member Migration
    
    * Parcial02 - Add Data seeding
    
    * Parcial02 - Add Member Repository skeleton
    
    * Parcial02 - Add Member repository
    
    * Parcial02 - Implement member repo in controller
    
    * Parcial02 - Add JsonIgnore to simulate mapper
    
    * Parcial02 - EXTRA - Custom auto mapper
    
    * Parcial - EXTRA - Enhance program.cs

---
+++ API/DTOs/RegisterRequest.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace API.DTOs;
+
+using System.ComponentModel.DataAnnotations;
+
+public class RegisterRequest
+{
+    [Required]
+    public string DisplayName { get; set; } = string.Empty;
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; } = string.Empty;
+}
