25b09518122ea113337ba2745ce42f6c0c1208c9
Angellunaa <angelluna996@gmail.com>
2025-10-30 07:37:03 -0600

    Parcial03 - Add members service

---
+++ client/src/core/services/members-service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Member } from '../../types/member';
+import { Observable } from 'rxjs';
+import { AccountService } from './account-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  private http = inject(HttpClient);
+  private baseUrl = environment.apiUrl;
+  private accountService = inject(AccountService);
+
+  getMember(id: string): Observable<Member> {
+    return this.http.get<Member>(this.baseUrl + "members/" + id, this.gethttpOptions());
+  }
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + "members", this.gethttpOptions());
+  }
+  private gethttpOptions(){
+    return {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + this.accountService.currentUser()?.token
+      })
+    };
+  }
+}
