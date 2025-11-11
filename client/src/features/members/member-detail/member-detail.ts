fe14b9a47ead9b1bc1097edf1029043ad3721516
Angellunaa <angelluna996@gmail.com>
2025-11-10 20:57:09 -0600

    Parcial03 - Add member detail page

--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -1,20 +1,19 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../../core/services/members-service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
-import { Observable } from 'rxjs';
 import { Member } from '../../../types/member';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, RouterLink, RouterLinkActive],
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css'
 })
-
-export class MemberDetail implements OnInit{
-  private memberService = inject(MembersService);
-  private route = inject(ActivatedRoute)
+export class MemberDetail implements OnInit {
+  private membersService = inject(MembersService);
+  private route = inject(ActivatedRoute);
   protected member$?: Observable<Member>;
 
   ngOnInit(): void {
@@ -23,7 +22,11 @@ export class MemberDetail implements OnInit{
 
   loadMember(): Observable<Member> | undefined {
     const id = this.route.snapshot.paramMap.get("id");
-    if(id) return this.memberService.getMember(id);
+
+    if (id) {
+      return this.membersService.getMember(id);
+    }
+
     return;
   }
 }
