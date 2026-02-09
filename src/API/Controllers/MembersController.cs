a7617c27c30cf8eed08a6a22b9971b9daefbefe0
Angellunaa <92132056+Angellunaa@users.noreply.github.com>
2026-02-08 18:28:28 -0600

    Parcial03 (#5)
    * Parcial03 - Add Member interface to SPA
    
    * Parcial03 - Add members service
    
    * Parcial03 - Add member list
    
    * Parcial03 - Add member cards
    
    * Parcial03 - Add style to member cards
    
    * Parcial03 - Add JWT Interceptor
    
    * Parcial03 - Add member detail page
    
    * Parcial03 - Add member detail page
    
    * Parcial03 - Add member detail child components
    
    Parcial03 - Add member detail child components
    
    * Parcial03 - Add member resolver
    
    * Parcial03 - Add Profile content
    
    * Parcial03 - Add Photos content
    
    * Parcial03 - Add Date pipe
    
    * Parcial03 - Add custom AGE pipe
    
    * Parcial03 - Add theme manager
    
    * Parcial03 - Add new folder structure for src and tests
    
    * Parcial03 - Add first test
    
    * Parcial03 - Add more unit test
    
    * Parcial03 - Update MembersController tests
    
    * Parcial03 - Add MembersRepositoyTests
    
    * Parcial03 - Add Integration Tests
    
    * Parcial03 - Add more integration tests

--- src/API/Controllers/MembersController.cs
+++ src/API/Controllers/MembersController.cs
@@ -1,24 +1,20 @@
-using API.Data;
 using API.Entities;
 using API.Interfaces;
 using API.Mappers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 [Authorize]
 public class MembersController(IMembersRepository memebersRepository) : BaseApiController
 {
-    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
     {
         return Ok(await memebersRepository.GetMembersAsync());
     }
 
-    [AllowAnonymous]
     [HttpGet("{id}")] // https://localhost:5001/api/members/bob-id
     public async Task<ActionResult<Member>> GetMember(string id)
     {
