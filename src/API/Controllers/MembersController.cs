4a0955f1c76a731d2a563d416a261b6edb5cd516
Angellunaa <92132056+Angellunaa@users.noreply.github.com>
2026-02-10 07:19:47 -0600

    Parcial04 - Add member update pesistance

--- src/API/Controllers/MembersController.cs
+++ src/API/Controllers/MembersController.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using API.Mappers;
@@ -7,18 +9,18 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 
 [Authorize]
-public class MembersController(IMembersRepository memebersRepository) : BaseApiController
+public class MembersController(IMembersRepository membersRepository) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
     {
-        return Ok(await memebersRepository.GetMembersAsync());
+        return Ok(await membersRepository.GetMembersAsync());
     }
 
     [HttpGet("{id}")] // https://localhost:5001/api/members/bob-id
     public async Task<ActionResult<Member>> GetMember(string id)
     {
-        var member = await memebersRepository.GetMemberAsync(id);
+        var member = await membersRepository.GetMemberAsync(id);
 
         if (member == null) return NotFound();
 
@@ -28,6 +30,37 @@ public class MembersController(IMembersRepository memebersRepository) : BaseApiC
     [HttpGet("{id}/photos")]
     public async Task<ActionResult<IReadOnlyList<Photo>>> GetPhotos(string id)
     {
-        return Ok(await memebersRepository.GetPhotosAsync(id));
+        return Ok(await membersRepository.GetPhotosAsync(id));
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateMember(MemberUpdateRequest request)
+    {
+        var memberId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (memberId == null)
+        {
+            return BadRequest("No id found in token");
+        }
+
+        var member = await membersRepository.GetMemberAsync(memberId);
+
+        if (member == null)
+        {
+            return BadRequest("Failed to get member");
+        }
+
+        member.DisplayName = request.DisplayName ?? member.DisplayName;
+        member.Description = request.Description ?? member.Description;
+        member.City = request.City ?? member.City;
+        member.Country = request.Country ?? member.Country;
+
+        membersRepository.Update(member);
+
+        if(await membersRepository.SaveAllAsync()) {
+            return NoContent();
+        }
+
+        return BadRequest("Failed to update member");
     }
 }
