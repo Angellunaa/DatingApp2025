b4da11090c1e8656fe01fb037742ffa8e9853462
Angellunaa <92132056+Angellunaa@users.noreply.github.com>
2026-03-15 17:47:12 -0600

    Parcial05 - Add client side registration

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,33 +1,44 @@
 import { Component, inject, output, signal } from '@angular/core';
 import { RegisterCreds } from '../../../types/user';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { AccountService } from '../../../core/services/account-service';
 import { JsonPipe } from '@angular/common';
-import { TextInput } from "../../../shared/text-input/text-input";
+import { TextInput } from '../../../shared/text-input/text-input';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   imports: [ReactiveFormsModule, JsonPipe, TextInput],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrl: './register.css',
 })
 export class Register {
   private accountService = inject(AccountService);
+  private router = inject(Router);
   private fb = inject(FormBuilder);
   protected creds = {} as RegisterCreds;
   protected credentialsForm: FormGroup;
   protected profileForm: FormGroup;
   protected currentStep = signal(1);
+  protected validationErrors = signal<string[]>([]);
   cancelRegister = output<boolean>();
 
   private readonly EMAIL_REGEX = '([\\w-+]+(?:\\.[\\w-+]+)*@(?:[\\w])+(?:\\.[a-zA-Z]{2,5}){1,2})';
-  
+
   constructor() {
     this.credentialsForm = this.fb.group({
       email: ['', [Validators.required, Validators.pattern(this.EMAIL_REGEX)]],
       displayName: ['', [Validators.required]],
       password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
-      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]],
     });
 
     this.profileForm = this.fb.group({
@@ -45,20 +56,20 @@ export class Register {
   matchValues(matchTo: string): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       const parent = control.parent;
-      if(!parent) return null;
+      if (!parent) return null;
       const matchValue = parent.get(matchTo)?.value;
       return control.value === matchValue ? null : { passwordMismatch: true };
-    }
+    };
   }
 
   nextStep() {
     if (this.credentialsForm.valid) {
-      this.currentStep.update(prevStep => prevStep + 1);
+      this.currentStep.update((prevStep) => prevStep + 1);
     }
   }
 
   prevStep() {
-    this.currentStep.update(prevStep => prevStep - 1);
+    this.currentStep.update((prevStep) => prevStep - 1);
   }
 
   getMaxDate() {
@@ -68,27 +79,21 @@ export class Register {
   }
 
   register(): void {
-    console.group("REGISTER");
-    console.log(this.credentialsForm.value);
-    console.log(this.profileForm.value);
-    console.log(this.currentStep());
-    console.groupEnd();
+    if (this.credentialsForm.valid && this.profileForm.valid) {
+      const formData = { ...this.credentialsForm.value, ...this.profileForm.value };
 
-    if(this.credentialsForm.valid && this.profileForm.valid) {
-      const FormData = {...this.credentialsForm.value, ...this.profileForm.value};
-
-      console.log('Form Data:', FormData);
+      this.accountService.register(formData).subscribe({
+        next: () => {
+          this.router.navigateByUrl('/members');
+        },
+        error: (error) => {
+          this.validationErrors.set(error);
+          console.log(error);
+        },
+      });
     }
-
-    //this.accountService.register(this.creds).subscribe({
-    //  next: response => {
-    //    console.log(response);
-    //    this.cancel();
-    //  },
-    //  error: error => console.log(error)
-    //});
   }
-  
+
   cancel(): void {
     this.cancelRegister.emit(false);
   }
