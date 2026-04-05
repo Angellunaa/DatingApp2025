a6fe1a0df6ecce300f9ad3d9190b2b12d0d0f638
Angellunaa <92132056+Angellunaa@users.noreply.github.com>
2026-04-05 11:53:24 -0600

    Parcial05 - Add action filter to log user activity

--- src/API/Controllers/BaseApiController.cs
+++ src/API/Controllers/BaseApiController.cs
@@ -1,7 +1,9 @@
+using API.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [ServiceFilter(typeof(UserActivityLogger))]
     [Route("api/[controller]")]
     [ApiController]
     public class BaseApiController : ControllerBase
