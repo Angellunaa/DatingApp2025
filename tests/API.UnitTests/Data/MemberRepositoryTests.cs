4fc8d193e6640f23e4faa56fbe4994782ab14f04
Angellunaa <92132056+Angellunaa@users.noreply.github.com>
2026-06-05 16:49:01 -0600

    Parcial06 - Add messages controller

--- tests/API.UnitTests/Data/MemberRepositoryTests.cs
+++ tests/API.UnitTests/Data/MemberRepositoryTests.cs
@@ -1,28 +1,31 @@
-using System.Threading.Tasks;
 using API.Data;
+using API.Entities
+using API.Helpers;
+using API.Interfaces;
 
 namespace API.UnitTests.Data;
 
 public class MemberRepositoryTests
 {
     private AppDbContext _context;
-    private MembersRepository _memberRepository;
+    private MembersRepository _membersRepository;
 
     [SetUp]
     public void Setup()
     {
         _context = GlobalTestSetup.AppDbContext;
-        _memberRepository = new MembersRepository(_context);
+        _membersRepository = new MembersRepository(_context);
     }
 
     [Test]
     public async Task Test1()
     {
         //Arrange & Act
-        var members = await _memberRepository.GetMembersAsync();
+        var memberRequest = new MemberRequest {};
+        var members = await _membersRepository.GetMembersAsync(memberRequest);
 
         //Assert
         Assert.That(members, Is.Not.Null);
-        Assert.That(members, Has.Count.EqualTo(10));
+        Assert.That(members.Items, Has.Count.EqualTo(10));
     }
 }
