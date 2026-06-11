3fca969f95fd30d6ce384684c17fcd9b89bab048
Angellunaa <92132056+Angellunaa@users.noreply.github.com>
2026-06-10 20:01:26 -0600

    Parcial06 - SignalR - Add presence tracker

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -102,7 +102,7 @@ public static class Program
                     ValidateIssuer = false,
                     ValidateAudience = false
                 };
-                
+
                 options.Events = new JwtBearerEvents
                 {
                     OnMessageReceived = context =>
@@ -144,6 +144,7 @@ public static class Program
         builder.Services.AddScoped<UserActivityLogger>();
         builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
         builder.Services.AddSignalR();
+        builder.Services.AddSingleton<PresenceTracker>();
     }
 
     private static void AddOpenApiDocument(WebApplicationBuilder builder)
